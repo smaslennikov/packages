474d003b82a9359438b8b3b623bb522d92d031d2
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-01-31 12:20:37 -0800

    Package ntbtls

---
+++ ntbtls/README.md
@@ -0,0 +1,4 @@
+# Slobber's Packages
+## GnuPG Dependency Package - `ntbtls`
+
+A decent version of GnuPG for RHEL/CentOS 7: ntbtls dependency
---
+++ ntbtls/recipe.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+class Ntbtls < FPM::Cookery::Recipe
+  name              'nbtls'
+  description       'GnuPG package for RHEL/CentOS 7: ntbtls dependency'
+  maintainer        'Svyatoslav I. Maslennikov <me@smaslennikov.com>'
+  section           'main'
+
+  version           '0.1.2'
+  revision          '1'
+  arch              'all'
+
+  source            "https://gnupg.org/ftp/gcrypt/ntbtls/ntbtls-#{version}.tar.bz2"
+  sha256            '8240db84e50c2351b19eb8064bdfd4d25e3c157d37875c62e335df237d7bdce7'
+
+  build_depends     'libgcrypt20-dev',
+                    'libksba-dev'
+
+  depends           'libgpg-error >= 1.17',
+                    'libgcrypt >= 1.6.0',
+                    'libassuan >= 2.5.0',
+                    'npth >= 1.2'
+
+  replaces          'ntbtls',
+                    'ntbtls-devel'
+  conflicts         'ntbtls',
+                    'ntbtls-devel'
+
+  def build
+    #configure :prefix => prefix, 'disable-install-doc' => true
+    configure
+    make
+  end
+
+  def install
+    #make :install, 'DESTDIR' => destdir
+    make :install
+  end
+end
