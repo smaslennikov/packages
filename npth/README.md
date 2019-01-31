81b61fde158a082f35376233664bfab690665e74
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-01-31 12:20:32 -0800

    Package npth

---
+++ npth/README.md
@@ -0,0 +1,4 @@
+# Slobber's Packages
+## GnuPG Dependency Package - `npth`
+
+A decent version of GnuPG for RHEL/CentOS 7: npth dependency
---
+++ npth/recipe.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+class Npth < FPM::Cookery::Recipe
+  name              'npth'
+  description       'GnuPG package for RHEL/CentOS 7: npth dependency'
+  maintainer        'Svyatoslav I. Maslennikov <me@smaslennikov.com>'
+  section           'main'
+
+  version           '1.6'
+  revision          '1'
+  arch              'all'
+
+  source            "https://gnupg.org/ftp/gcrypt/npth/npth-#{version}.tar.bz2"
+  sha256            '1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1'
+
+  replaces          'npth',
+                    'npth-devel'
+  conflicts         'npth',
+                    'npth-devel'
+
+  def build
+    #configure :prefix => prefix, 'disable-install-doc' => true
+    configure
+    make
+  end
+
+  def install
+    #make :install, 'DESTDIR' => destdir
+    make :install
+  end
+end
