526f9e03291bd244902a56976cc47abcb97a819e
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-01-31 12:20:25 -0800

    Package libksba

---
+++ libksba/README.md
@@ -0,0 +1,4 @@
+# Slobber's Packages
+## GnuPG Dependency Package - `libksba`
+
+A decent version of GnuPG for RHEL/CentOS 7: libksba dependency
---
+++ libksba/recipe.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+class Libksba < FPM::Cookery::Recipe
+  name              'libksba'
+  description       'GnuPG package for RHEL/CentOS 7: libksba dependency'
+  maintainer        'Svyatoslav I. Maslennikov <me@smaslennikov.com>'
+  section           'main'
+
+  version           '1.3.5'
+  revision          '1'
+  arch              'all'
+
+  source            "https://gnupg.org/ftp/gcrypt/libksba/libksba-#{version}.tar.bz2"
+  sha256            '41444fd7a6ff73a79ad9728f985e71c9ba8cd3e5e53358e70d5f066d35c1a340'
+
+  replaces          'libksba',
+                    'libksba-devel'
+  conflicts         'libksba',
+                    'libksba-devel'
+
+  def build
+    #configure :prefix => prefix, 'disable-install-doc' => true
+    configure
+    make
+  end
+
+  def install
+    #make :install, 'DESTDIR' => destdir
+    make :install
+  end
+end
