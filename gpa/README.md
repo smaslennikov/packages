561e63a32030987a4604f2d702e7225d4f4548cf
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-01-31 12:20:50 -0800

    Package gpa

---
+++ gpa/README.md
@@ -0,0 +1,4 @@
+# Slobber's Packages
+## GnuPG Optional Dependency Package - `gpa`
+
+A decent version of GnuPG for RHEL/CentOS 7: optional gpa dependency
---
+++ gpa/recipe.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+class Gpa < FPM::Cookery::Recipe
+  name              'gpa'
+  description       'GnuPG package for RHEL/CentOS 7: optional gpa dependency'
+  maintainer        'Svyatoslav I. Maslennikov <me@smaslennikov.com>'
+  section           'main'
+
+  version           '0.10.0'
+  revision          '1'
+  arch              'all'
+
+  source            "https://gnupg.org/ftp/gcrypt/#{name}/#{name}-#{version}.tar.bz2"
+  sha256            '95dbabe75fa5c8dc47e3acf2df7a51cee096051e5a842b4c9b6d61e40a6177b1'
+
+  build_depends     'libgtk2.0-dev',
+                    'libgpgme-dev'
+
+  replaces          'gpa',
+                    'gpa-devel'
+  conflicts         'gpa',
+                    'gpa-devel'
+
+  def build
+    #configure :prefix => prefix, 'disable-install-doc' => true
+    configure
+    make
+  end
+
+  def install
+    #make :install, 'DESTDIR' => destdir
+    make :install
+  end
+end
