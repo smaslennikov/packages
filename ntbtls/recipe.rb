4f77ce51ca59ae0c5846f34984a85a92a429255e
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-02-01 11:49:58 -0800

    Fix build depends for ntbtls

--- ntbtls/recipe.rb
+++ ntbtls/recipe.rb
@@ -13,20 +13,22 @@ class Ntbtls < FPM::Cookery::Recipe
   source            "https://gnupg.org/ftp/gcrypt/#{name}/#{name}-#{version}.tar.bz2"
   sha256            '8240db84e50c2351b19eb8064bdfd4d25e3c157d37875c62e335df237d7bdce7'
 
-  platforms [:centos, :redhat] do
-    build_depends     'libgcrypt18',
-                      'libksba-dev'
-  end
+  build_depends     'libgcrypt',
+                    'libksba-dev'
 
   platforms [:ubuntu] do
-    build_depends     'libgcrypt',
-                      'libksba-dev'
+    depends           'libgpg-error >= 1.17',
+                      'libgcrypt',
+                      'libassuan >= 2.5.0',
+                      'npth >= 1.2'
   end
 
-  depends           'libgpg-error >= 1.17',
-                    'libgcrypt18',
-                    'libassuan >= 2.5.0',
-                    'npth >= 1.2'
+  platforms [:centos, :redhat] do
+    depends           'libgpg-error >= 1.17',
+                      'libgcrypt18',
+                      'libassuan >= 2.5.0',
+                      'npth >= 1.2'
+  end
 
   replaces          'ntbtls',
                     'ntbtls-devel'
